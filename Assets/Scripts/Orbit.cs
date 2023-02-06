88cba91a0367145e1250b9b395a2b9fb29d69d02
Daniel Ilett <danielilett@gmail.com>
2023-02-06 11:58:33 +0000

    Improved shield effect.
    The glow is a lot less complicated and looks far better now. It is a global glowing pulse effect rather than a clouds-based scrolling texture combination.

---
+++ Assets/Scripts/Orbit.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour
+{
+    [SerializeField] private Transform orbitObject;
+    [SerializeField] private float orbitSize;
+    [SerializeField] private float orbitOffset;
+    [SerializeField] private float orbitSpeed;
+
+    private void Update()
+    {
+        if (!Input.GetButton("Fire1"))
+        {
+            var time = Time.time * Mathf.PI * orbitSpeed;
+
+            var xPos = orbitObject.position.x + Mathf.Sin(time) * orbitSize;
+            var yPos = orbitObject.position.y + orbitOffset;
+            var zPos = orbitObject.position.z + Mathf.Cos(time) * orbitSize;
+
+            transform.position = new Vector3(xPos, yPos, zPos);
+            transform.LookAt(orbitObject);
+        }
+    }
+}
